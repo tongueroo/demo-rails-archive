a4f3f378b31be47ef5bf6bce07353e4b2a6ae0f6
Tung Nguyen <tongueroo@gmail.com>
2018-01-19 16:51:26 -0800

    update readme

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,12 @@
 class DemoController < ApplicationController
   def index
+    @tables = ActiveRecord::Base.connection.tables
+    @db_version = db_version
+  end
+
+  private
+  def db_version
+    # https://makandracards.com/makandra/42104-rails-how-to-get-postgresql-version-being-used
+    ActiveRecord::Base.connection.select_value('SELECT version()')
   end
 end
