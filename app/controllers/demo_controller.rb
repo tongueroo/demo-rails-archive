08db9c75dcfa472ddb66b247aaaefa4d9f35ccb0
Tung Nguyen <tongueroo@gmail.com>
2018-10-28 12:31:40 +0000

    add info route

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,11 +2,17 @@ class DemoController < ApplicationController
   def index
     @tables = ActiveRecord::Base.connection.tables
     @db_version = db_version
+    @db_adapter = ActiveRecord::Base::connection.class
   end
 
   private
   def db_version
-    # https://makandracards.com/makandra/42104-rails-how-to-get-postgresql-version-being-used
-    ActiveRecord::Base.connection.select_value('SELECT version()')
+    case ActiveRecord::Base::connection
+    when ActiveRecord::ConnectionAdapters::SQLite3Adapter
+      ActiveRecord::Base.connection.select_value('SELECT sqlite_version()') # works for sqlite
+    else
+      # https://makandracards.com/makandra/42104-rails-how-to-get-postgresql-version-being-used
+      ActiveRecord::Base.connection.select_value('SELECT version()') # works for postgresql
+    end
   end
 end
